f494bdb9ea70484c49da59da59027deee2002997
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-23 12:42:38 +0100

    add: create register user and login function in the authController

---
+++ src/controllers/authController.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-undef */
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const bankUsers = [];
+
+const registerNew = async (req, res) => {
+
+    const { fullName, email, phone, password, role } = req.body
+
+    const existingUser = bankUsers.find(users => users.email === email)
+    if (existingUser) {
+        return res.json({
+            status: "error",
+            message: "This email already exists"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(
+        password,
+        Number(process.env.SALT_ROUNDS)
+    );
+
+    const newUser = {
+
+        id: bankUsers.length + 1,
+        fullName,
+        email,
+        phone,
+        password: hashedPassword,
+        accountNumber: Math.random() * 10,
+        role
+    }
+
+    bankUsers.push(newUser)
+    console.log(bankUsers)
+
+    return res.status(201).json({
+        status: "sucessful",
+        message: "You have registered successfully"
+    })
+
+}
+
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body
+
+    const existingUser = bankUsers.find(users => users.email === email)
+    if (!existingUser) {
+        return res.status(404).json({
+            status: "error",
+            message: "This email already exists"
+        })
+    }
+
+
+    const passwordMatch = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+
+   if(!passwordMatch){
+    return res.status(409).json({
+        status: "error",
+        message: "incorrect password"
+    })   }
+
+    const token = jwt.sign(
+        {
+            id: existingUser.id,
+            email: existingUser.email,
+            role: existingUser.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1h"
+        }
+    );
+
+    return res.status(201).json({
+        status: "sucessful",
+        message: "You have been successfully logged in",
+        token
+    });
+
+}
+
+
+module.exports = {
+    registerNew,
+    loginUser
+};
