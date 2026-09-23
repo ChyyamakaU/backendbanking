97f316ee233b2bc7e048bbc3f88604779cf82808
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-23 11:21:50 +0100

    ft: add readme for features of the banking system

---
+++ README.md
@@ -0,0 +1,6 @@
+
+// Build a banking system
+// 1. user can register and login
+// 2.user can view their balance
+// 3. user can create pin and update pin
+// 4.user can make transfer to other user
--- src/validators/user.js
+++
@@ -1,7 +0,0 @@
-// const { useFormState } = require("react-dom")
-
-// Build a banking system
-// 1. user can register and login
-// 2.user can view their balance
-// 3. user can create pin and update pin
-// 4.user can make transfer to other user
