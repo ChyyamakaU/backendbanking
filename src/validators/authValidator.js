9eac7e6dbf78bab91aaab2d6dab3a797c86161fe
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-23 12:47:26 +0100

    ft: add validation for registeration in auth validator

---
+++ src/validators/authValidator.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+
+const validateRegister = (req, res, next) => {
+  const { name, email, phone, password, role } = req.body;
+
+  if (!name || !email || !phone, !password || !role) {
+
+    return res.status(400).json({
+      status: "error",
+      message: "Name, email, password and role are required"
+    });
+  }
+
+ 
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Please provide a valid email"
+    });
+  }
+
+
+  const passwordPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/;
+
+  if (!passwordPattern.test(password)) {
+    return res.status(400).json({
+      status: "error",
+      message:
+        "Password must be at least 6 characters and contain uppercase, lowercase, number and special character"
+    });
+  }
+
+  if (role !== "admin" && role !== "user") {
+    return res.status(400).json({
+      status: "error",
+      message: "Role must be either admin or user"
+    });
+  }
+
+  next();
+};
+
+
+
+
+module.exports = {
+  validateRegister
+};
