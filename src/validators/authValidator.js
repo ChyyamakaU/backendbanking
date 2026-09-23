af2dcf0462bdb8776d6b89298eb6e3ffc44bd5b2
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-23 12:48:15 +0100

    ft: add the validate login in the auth validator

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -44,8 +44,20 @@ const validateRegister = (req, res, next) => {
 };
 
 
+const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email and password are required"
+    });
+  }
+
+  next();
+};
 
 module.exports = {
-  validateRegister
+  validateRegister,
+  validateLogin
 };
